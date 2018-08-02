a762da3b9991385dfb5a5c8a9caf04f262505886
Griffen <griffencroft1@gmail.com>
2018-08-02 13:20:37 -0700

    styling start

--- web/src/components/general/NavBar.js
+++ web/src/components/general/NavBar.js
@@ -3,47 +3,76 @@ import { Link } from 'react-router-dom';
 import ConsumerLanding from './../consumer/ConsumerLanding';
 import ChefLanding from './../chef/ChefLanding';
 import GeneralLanding from './GeneralLanding';
+import { Menu } from 'semantic-ui-react';
+import './../../../public/index.css'
+export default class NavBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      activeItem: 'messages',
+    }
+  }
+  handleItemClick = (e, { name }) => this.setState({activeItem: name});
 
-function nav(role, id){
-  if (role === "consumer") {
+  nav = (role, id) => {
+    const { activeItem } = this.state
+    if (role === "consumer") {
+      return (
+        <div>
+          <Menu text className="navBar">
+            <Link to="/request" >
+              <Menu.Item name='Request' active={ activeItem === 'request'} />
+            </Link>
+            <Link to="/messages">
+              <Menu.Item name='Messages' active={ activeItem === 'messages'} />
+            </Link>
+            <Link to={`/user/${id}`}>
+              <Menu.Item name='Profile' active={ activeItem === 'profile'} />
+            </Link>
+          </Menu>
+        </div>
+      )
+    } else if (role === "chef") {
+      return (
+        <div>
+          <Menu text className="navBar">
+            <Link to='/'>
+              <Menu.Item name='Messages' active={ activeItem === 'messages'} />
+            </Link>
+            <Link to='/'>
+              <Menu.Item name='Profile' active={ activeItem === 'profile'} />
+            </Link>
+            <Link to='/'>
+              <Menu.Item name='Consumer Mode' active={ activeItem === 'consumer'} />
+            </Link>
+          </Menu>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Menu className="navBar">
+            <Link to='/'>
+              <Menu.Item name='About' active={ activeItem === 'about'} />
+            </Link>
+            <Link to='/'>
+              <Menu.Item name='Become a Chef' active={ activeItem === 'becomeChef'} />
+            </Link>
+            <Link to='/signup'>
+              <Menu.Item name='Sign up' active={ activeItem === 'signup'} />
+            </Link>
+            <Link to='/'>
+              <Menu.Item name='Log In' active={ activeItem === 'logIn'} />
+            </Link>
+          </Menu>
+        </div>
+      )
+    }
+  }
+  render(){
+
     return (
-      <div>
-        <Link to="/request" >Request</Link>
-        <Link to="/messages">Messages</Link>
-        <Link to={`/user/${id}`}>Profile</Link>
-      </div>
-    )
-  } else if (role === "chef") {
-    return (
-      <div>
-        <Link to="/messages">Messages</Link>
-        <Link to={`/user/${id}`}>Profile</Link>
-        <Link to='/'>Consumer Mode</Link>
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <Link to='/'>About</Link>
-        <Link to='/'>Become a Chef</Link>
-        <Link to='/'>Sign up</Link>
-        <Link to='/'>Log in</Link>
-      </div>
+      <div className="flex-container">{this.nav(this.state.role, this.state.id)}</div>
     )
   }
 }
-
-export default class NavBar extends Component{
-  state = { role: this.props.user.role, id: this.props.user._id}
-
-  render(){
-    console.log(this.props);
-    return (
-      <div>{nav(this.state.role, this.state.id)}</div>
-    )
-    // const user = this.props.location.state.user;
-    // if (user.role === "chef") return <ChefLanding/>
-    // else if (user.role === "consumer") return <ConsumerLanding/>
-    // else return <GeneralLanding/>;
-  }
-};
