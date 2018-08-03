d99867a9c08198506723f2535e72ecb2534ce623
Dhruva Mehta <30417337+dhruva-mehta@users.noreply.github.com>
2018-08-03 10:51:12 -0700

    fixed async map location loading

--- web/src/components/maps/MapContainer.jsx
+++ web/src/components/maps/MapContainer.jsx
@@ -12,12 +12,15 @@ const {
 import SearchBox from "react-google-maps/lib/components/places/SearchBox"
 const { MarkerWithLabel } = require("react-google-maps/lib/components/addons/MarkerWithLabel");
 
-const MapWithASearchBox = geolocated({
-  positionOptions: {
-    enableHighAccuracy: false,
-  },
-  userDecisionTimeout: 5000,
-})(compose(
+// var coords = geolocated({
+//   positionOptions: {
+//     enableHighAccuracy: false,
+//   },
+// })
+
+var geo = window.navigator.geolocation
+
+var MapWithLocation = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDM7lCbSzaudr_c9xSve0RiF7Zl5UX2rac&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
@@ -25,10 +28,24 @@ const MapWithASearchBox = geolocated({
     mapElement: <div style={{ height: `100%` }} />,
   }),
   lifecycle({
+    componentDidMount() {
+      var self = this
+      console.log(this)
+      geo.getCurrentPosition(function(position) {
+        if (position) {
+          self.setState({
+            center: {lat: position.coords.latitude, lng: position.coords.longitude}
+          })
+        } else {
+          console.log('boo')
+        }
+      })
+    },
     componentWillMount() {
       const refs = {}
       this.setState({
         bounds: null,
+        center: {lat: 0, lng: 0},
         markers: [],
         onMapMounted: ref => {
           refs.map = ref;
@@ -69,57 +86,57 @@ const MapWithASearchBox = geolocated({
   }),
   withScriptjs,
   withGoogleMap
-)((props) => {
-  return (
-  <GoogleMap
-    ref={props.onMapMounted}
-    defaultZoom={15}
-    center={{lat: props.coords.latitude, lng: props.coords.longitude}}
-    onBoundsChanged={props.onBoundsChanged}
-  >
-    <MarkerWithLabel
-      position={{lat: props.coords.latitude, lng: props.coords.longitude}}
-      labelAnchor={new window.google.maps.Point(0, 0)}
-      labelStyle={{backgroundColor: "purple", fontSize: "28px", padding: "16px"}}
-    >
-      <div>This is you!</div>
-    </MarkerWithLabel>
-    <SearchBox
-      ref={props.onSearchBoxMounted}
-      bounds={props.bounds}
-      controlPosition={window.google.maps.ControlPosition.TOP_LEFT}
-      onPlacesChanged={props.onPlacesChanged}
-    >
-      <input
-        type="text"
-        placeholder="Customized your placeholder"
-        style={{
-          boxSizing: `border-box`,
-          border: `1px solid transparent`,
-          width: `240px`,
-          height: `32px`,
-          marginTop: `27px`,
-          padding: `0 12px`,
-          borderRadius: `3px`,
-          boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-          fontSize: `14px`,
-          outline: `none`,
-          textOverflow: `ellipses`,
-        }}
-      />
-    </SearchBox>
-    {props.markers.map((marker, index) =>
-      <MarkerWithLabel
-        key={index}
-        position={marker.position}
-        labelAnchor={new window.google.maps.Point(0, 0)}
-        labelStyle={{backgroundColor: "yellow", fontSize: "32px", padding: "16px"}}
+  )((props) => {
+    return (
+      <GoogleMap
+        ref={props.onMapMounted}
+        defaultZoom={15}
+        center={props.center}
+        onBoundsChanged={props.onBoundsChanged}
       >
-        <div>Hello There!</div>
-      </MarkerWithLabel>
-    )}
-  </GoogleMap>
-)
-})
-);
-export default MapWithASearchBox
+        <MarkerWithLabel
+          position={props.center}
+          labelAnchor={new window.google.maps.Point(0, 0)}
+          labelStyle={{backgroundColor: "purple", fontSize: "28px", padding: "16px"}}
+        >
+          <div>This is you!</div>
+        </MarkerWithLabel>
+        <SearchBox
+          ref={props.onSearchBoxMounted}
+          bounds={props.bounds}
+          controlPosition={window.google.maps.ControlPosition.TOP_LEFT}
+          onPlacesChanged={props.onPlacesChanged}
+        >
+          <input
+            type="text"
+            placeholder="Customized your placeholder"
+            style={{
+              boxSizing: `border-box`,
+              border: `1px solid transparent`,
+              width: `240px`,
+              height: `32px`,
+              marginTop: `27px`,
+              padding: `0 12px`,
+              borderRadius: `3px`,
+              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+              fontSize: `14px`,
+              outline: `none`,
+              textOverflow: `ellipses`,
+            }}
+          />
+        </SearchBox>
+        {props.markers.map((marker, index) =>
+          <MarkerWithLabel
+            key={index}
+            position={marker.position}
+            labelAnchor={new window.google.maps.Point(0, 0)}
+            labelStyle={{backgroundColor: "yellow", fontSize: "32px", padding: "16px"}}
+          >
+            <div>Hello There!</div>
+          </MarkerWithLabel>
+        )}
+      </GoogleMap>
+    )
+  });
+
+export default MapWithLocation
